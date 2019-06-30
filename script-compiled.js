3877af3031648045a49f9744d1fea815f3cc46e9
Bartek Błęcki <bartek@blecki.pl>
2019-06-30 22:14:04 +0200

    Exercise 2

--- script-compiled.js
+++ script-compiled.js
@@ -5,3 +5,11 @@ var hello = 'Hello';
 var world = 'World';
 
 console.log(hello + ' ' + world);
+
+// Exercise 2
+var multiply = function multiply(a) {
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return a * b;
+};
+
+console.log(multiply(5));
--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let hello = 'Hello';
 let world = 'World';
 
 console.log(`${hello} ${world}`);
+
+// Exercise 2
+const multiply = (a, b = 1) => a * b;
+
+console.log(multiply(5));
